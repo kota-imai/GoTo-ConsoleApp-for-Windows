d181e58ae8e6f97171c7682958c34148c6aa9e6b
kota <kota,imai@farmimai.com>
2022-03-16 17:33:27 +0900

    プロジェクト ファイルを追加します。

---
+++ GoTo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GoTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("search word is invalid");
+                return;
+            }
+            string query = string.Join(" ", args);
+            string encoded = System.Web.HttpUtility.UrlEncode(query);
+            string url = "https://www.google.com/search?q=" + encoded + "&btnI";
+            ProcessStartInfo pi = new ProcessStartInfo()
+            {
+                FileName = url,
+                UseShellExecute = true,
+            };
+            Process.Start(pi);
+        }
+    }
+}
